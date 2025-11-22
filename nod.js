fa1e0e84ed48c374af6d897eb6edd1dccd485701
Jorens <jorensmerenjanu@gmail.com>
2025-11-22 21:22:46 +0200

    feat: device list and names

--- nod.js
+++ nod.js
@@ -2,13 +2,15 @@ const server = require('./lib/server');
 const client = require('./lib/client');
 
 const mode = process.argv[2];
+const command = process.argv[3];
+const commandArg = process.argv[4];
 
 if(mode === 'server') {
     server.start();
 } else {
-    client.ping().then((status) => {
-        if(status) {
-            console.log('pinged server successfully');
-        }
-    })
+    // console.log('running command ' + command);
+    client[command](commandArg).then((res) => {
+        // console.log('command resolved');
+        console.log(res);
+    });
 }
