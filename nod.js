886267329fbf3b7aa38d88d769f3c3dca07a067b
Jorens <jorensmerenjanu@gmail.com>
2025-11-23 15:41:08 +0200

    fix: socket not connecting

--- nod.js
+++ nod.js
@@ -1,5 +1,5 @@
 const server = require('./lib/server');
-const client = require('./lib/nodeClient');
+const NodeClient = require('./lib/nodeClient');
 
 const mode = process.argv[2];
 const command = process.argv[3];
@@ -8,6 +8,7 @@ const commandArg = process.argv[4];
 if(mode === 'server') {
     server.start();
 } else {
+    const client = new NodeClient();
     (async () => {
         await client.connect('client1');
         console.log('running command ' + command);
