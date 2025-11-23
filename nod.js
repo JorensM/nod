ab83ed436d1ab965e3ad9f7c2b0deca693d3addf
Jorens <jorensmerenjanu@gmail.com>
2025-11-23 16:10:45 +0200

    feat: ability to list shared folder contents

--- nod.js
+++ nod.js
@@ -3,20 +3,16 @@ const NodeClient = require('./lib/nodeClient');
 
 const mode = process.argv[2];
 const command = process.argv[3];
-const commandArg = process.argv[4];
+const commandArgs = process.argv.slice(4);//[4];
 
 if(mode === 'server') {
     server.start();
 } else {
     const client = new NodeClient();
-    (async () => {
-        await client.connect('client1');
+    client.onConnect = async () => {
         console.log('running command ' + command);
-        const res = await client[command](commandArg);//.then((res) => {
+        const res = await client[command](...commandArgs);//.then((res) => {
         console.log(res);
-        //     // console.log('command resolved');
-        //     console.log(res);
-        // });
-
-    })();
+    }
+    client.connect('client1');
 }
